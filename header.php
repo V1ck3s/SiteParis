abc9fb47ebd04f8504d9a232eccb2bd90ce93a58
vic1997 <vic-du-68@live.fr>
2018-07-17 13:10:22 +0200

    Projet terminé
    Le projet est terminé et mis en ligne.

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+
+	var_export($_POST);
+	exit();
+?>
+
+<html>
+	<head>
+		<title>Papawy Sports Bet</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<meta name="google-site-verification" content="-OCWIqZiDm8eS9sBWqR9ifZW6BKQxMQZSBQ130puBUA" />
+		<meta name="google-site-verification" content="9ibuz_jCYsmO3Ew9r42eS_JimEKEEZFzCea7mF4Yojw" />
+
+		<meta name="description" content="I'm a student and programmer.">
+  		<meta name="keywords" content="Papawy,Papanon,,SAMP,SA-MP-FR,sa-mp-fr,parier,foot,csgo,sport">
+  		<meta name="author" content="Papawy">
+
+		<!--[if lte IE 8]><script src="assets/js/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" type="image/png" href="/images/favicon.png">
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="/assets/css/noscript.css" /></noscript>
+
+		
+	</head>
+<html>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php //session_start(); ?>
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+
+<?php
+	include("utils/header.php");
+?>
+
+<html>
+<style> 
+body {
+    color: red;
+}
+</style>
+	<body class="is-loading">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						
+							
+					<?php
+						if(isset($_SESSION['login']))
+						{
+							
+							header('Location:Controleur/ctrl_accueil_paris.php');
+						}
+						else
+						{
+							include('Vue/vue_connexion_membre.php');
+						}
+					?>
+							
+						
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<!--
+						<ul class="copyright">
+							<li>&copy; Jane Doe</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					-->
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/respond.min.js"></script><![endif]-->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
